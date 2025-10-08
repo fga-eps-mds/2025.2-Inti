3b3ab5a72a2f3c9eef9ff41cfad188c595c7409a
gustav <gustavuh0012@gmail.com>
2025-10-08 19:19:16 -0300

    feat: mapeando as tabelas de memberships,likes,posts e shareds

---
+++ src/main/java/br/mds/inti/models/Likes.java
@@ -0,0 +1,43 @@
+package br.mds.inti.models;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "likes")
+public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = true)
+    private UUID user_id;
+
+    
+    @Column(name = "post_id", nullable = true)
+    private UUID post_id;
+
+    @Column(name = "created_at", nullable = true)
+    private Instant created_at;
+
+}
