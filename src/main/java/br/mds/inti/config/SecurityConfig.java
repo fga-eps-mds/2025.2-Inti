875694d16ed4d5ef5d9b62409fece28866e1b0ae
Lucas Santos <kelytonlucas@gmail.com>
2025-10-11 20:42:58 -0300

    fix: utilizando Scrypt da biblioteca bouncy cast

--- src/main/java/br/mds/inti/config/SecurityConfig.java
+++ src/main/java/br/mds/inti/config/SecurityConfig.java
@@ -10,8 +10,9 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -42,7 +43,8 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new SCryptPasswordEncoder(16384, 8, 1, 32, 16);
+        // return new BCryptPasswordEncoder();
     }
 
     @Bean
