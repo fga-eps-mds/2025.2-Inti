95b36394d6b69eced2168b0e7dd04564a255419b
Lucas Santos <kelytonlucas@gmail.com>
2025-11-01 18:13:07 -0300

    feat: add react-native-size-matters for responsive design in PerfilScreen

--- src/screens/app/PerfilScreen.tsx
+++ src/screens/app/PerfilScreen.tsx
@@ -1,33 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { useAuth } from '../../hooks/useAuth';
-
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { scale } from 'react-native-size-matters';
 
 export default function PerfilScreen() {
   return (
-    <ScrollView style={styles.container}>
-      {/* Foto de perfil */}
-      <Image
-        source={{
-          uri: 'https://images.unsplash.com/photo-1566843972705-1aad0ee32f88?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        }}
-        style={styles.profileImage}
-      />
-      {/* Nome */}
-      <Text style={styles.name}>BRUNNO GABRYEL</Text>
-      {/* Username */}
-      <Text style={styles.username}>@BRUNNINHO</Text>
-      {/* Informações de contato */}
-      <View style={styles.contactInfo}>
-        <Text style={styles.contactText}>(61) 99999-9999</Text>
-        <Text style={styles.contactText}>MUSA@ALUNO.UNB.BR</Text>
-      </View>
-      {/* Bio/Descrição */}rr
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.containerProfile}>
+          <Image
+            source={{
+              uri: 'https://images.unsplash.com/photo-1566843972705-1aad0ee32f88?q=80&w=1470&auto=format&fit=crop',
+            }}
+            style={styles.profileImage}
+          />
+
+          <View style={styles.nameContainer}>
+            <Text style={styles.name}>BRUNNO Gabryel</Text>
+            <Text style={styles.username}>@BRUNNINHO</Text>
+          </View>
+        </View>
+        <View style={styles.contactInfo}>
+          <Text style={styles.contactText}>(61) 99999-9999</Text>
+          <Text style={styles.contactText}>MUSA@ALUNO.UNB.BR</Text>
+          <Text style={styles.contactText}>Estudante de Direito</Text>
+        </View>
+        {/* 
+
       <Text style={styles.bio}>ESTUDANTE DE DIREITO</Text>
-      {/* Aqui viriam os posts */}
+
       <View style={styles.postsSection}>
         <Text style={styles.postsTitle}>POSTS</Text>
-        {/* Grid de posts viria aqui */}
+      </View> */}
       </View>
     </ScrollView>
   );
@@ -35,62 +38,70 @@ export default function PerfilScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    paddingTop: 49,
     paddingHorizontal: 20,
   },
+  header: {
+    width: '100%',
+    marginBottom: 20,
+    gap: 12,
+  },
+  containerProfile: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+
   profileImage: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    alignSelf: 'stretch',
-    marginTop: 20,
-    marginBottom: 16,
-    // Adicione border se tiver no design
-    // borderWidth: 2,
-    // borderColor: '#E0E0E0',
+    width: scale(100),
+    height: scale(100),
+    borderRadius: scale(100),
+    marginRight: 20, // espaço entre imagem e textos
+  },
+  nameContainer: {
+    flex: 1,
+    alignContent: 'center',
   },
   name: {
-    fontSize: 24,
-    fontWeight: 'light',
-    textAlign: 'left',
-    color: '#000000',
-    marginBottom: 4,
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: '600',
+    color: '#000',
     fontFamily: 'JuliusSansOne-Regular',
   },
   username: {
     fontSize: 16,
-    textAlign: 'left',
-    color: '#666666',
-    marginBottom: 16,
+    color: '#666',
+    textAlign: 'center',
   },
   contactInfo: {
     marginBottom: 16,
-    alignItems: 'flex-start', // ou 'flex-start' se for alinhado à esquerda
   },
   contactText: {
     fontSize: 14,
-    color: '#333333',
+    color: '#333',
     marginBottom: 4,
-    alignItems: 'flex-start',
   },
   bio: {
     fontSize: 16,
     textAlign: 'center',
-    color: '#333333',
-    fontStyle: 'italic', // se for o caso
+    color: '#333',
+    fontStyle: 'italic',
     marginBottom: 24,
   },
   postsSection: {
     borderTopWidth: 1,
     borderTopColor: '#E0E0E0',
+    width: '90%',
     paddingTop: 16,
+    alignItems: 'center',
   },
   postsTitle: {
     fontSize: 18,
     fontWeight: 'bold',
-    color: '#000000',
+    color: '#000',
     marginBottom: 16,
   },
 });
-
