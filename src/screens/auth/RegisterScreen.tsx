03ec72a48fc3234256c0c277408cab8dfc17a8a5
Natan8643 <natan.j.franca@gmail.com>
2025-11-04 19:08:35 -0300

    [STYLE]: Finaliza o front da pagina de cadastro

--- src/screens/auth/RegisterScreen.tsx
+++ src/screens/auth/RegisterScreen.tsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Alert, Pressable } from 'react-native';
 import { AuthScreenProps } from '../../@types/navigation';
 import { useAuth } from '../../hooks/useAuth';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 export default function RegisterScreen({ navigation }: AuthScreenProps<'Register'>) {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -56,74 +59,100 @@ export default function RegisterScreen({ navigation }: AuthScreenProps<'Register
         <View style={styles.container}>
             <Text style={styles.title}>Cadastro</Text>
 
-            {/* <View style={styles.form}> */}
-                <Text>Username</Text>
+            <View style={styles.form}>
+                <Text style={styles.titileInput}>Nome</Text>
                 <TextInput
                     style={[
                         styles.input,
                     ]}
                     placeholder="Ex: Musa UnB"
+                    value={name}
+                    placeholderTextColor={'#592E838C'}
+                    onChangeText={setName}
+                    autoCapitalize="none"
+                />
+                <Text style={styles.titileInput}>Username</Text>
+                <TextInput
+                    style={[
+                        styles.input,
+                    ]}
+                    placeholder="Ex: musa123"
                     value={username}
+                    placeholderTextColor={'#592E838C'}
                     onChangeText={setUsername}
                     autoCapitalize="none"
                 />
-
+                <Text style={styles.titileInput}>Email</Text>
                 <TextInput
                     style={styles.input}
-                    placeholder="Email"
+                    placeholder="Ex: musa@aluno.unb.br"
                     value={email}
                     onChangeText={setEmail}
+                    placeholderTextColor={'#592E838C'}
                     keyboardType="email-address"
                     autoCapitalize="none"
                 />
+                <Text style={styles.titileInput}>Senha</Text>
                 <TextInput
                     style={styles.input}
                     placeholder="Senha"
                     value={password}
+                    placeholderTextColor={'#592E838C'}
                     onChangeText={setPassword}
                     secureTextEntry={!showPassword}
                 />
+                <Text style={styles.titileInput}>Confirmar senha</Text>
                 <TextInput
                     style={styles.input}
                     placeholder="Confirmar Senha"
                     value={confirmPassword}
+                    placeholderTextColor={'#592E838C'}
                     onChangeText={setConfirmPassword}
                     secureTextEntry={!showPassword}
                 />
-            {/* </View> */}
-
-            <View style={styles.inputCheckbox}>
-                <Pressable
-                    style={[
-                        styles.checkbox,
-                        showPassword && styles.checkboxChecked
-                    ]}
-                    onPress={() => {
-                        setShowPassword(!showPassword)
-                        // console.log('showPassword:', !showPassword);
-
-                    }}
-                />
-
-                <Text style={styles.textCheckbox}>Mostrar senha</Text>
             </View>
 
-            <View style={styles.inputCheckbox}>
-                <Pressable
-                    style={[
-                        styles.checkbox,
-                        accountType && styles.checkboxChecked
-                    ]}
-                    onPress={() => setAccountType(!accountType)}
-                />
-                <Text style={styles.textCheckbox}>Sua conta é organizacional?</Text>
+            <View style={styles.containerCheckbox}>
+                <View style={styles.inputCheckbox}>
+                    <Pressable
+                        style={[
+                            styles.checkbox,
+                            showPassword && styles.checkboxChecked
+                        ]}
+                        onPress={() => setShowPassword(prev => !prev)}
+                    >
+                        {showPassword && (
+                            <FontAwesome name="check" size={16} color="#fff" />
+                        )}
+                    </Pressable>
+
+                    <Text style={styles.textCheckbox}>Mostrar senha</Text>
+                </View>
+
+                <View style={styles.inputCheckbox}>
+                    <Pressable
+                        style={[
+                            styles.checkbox,
+                            accountType && styles.checkboxChecked
+                        ]}
+                        onPress={() => setAccountType(!accountType)}
+                    >
+                        {accountType && (
+                            <FontAwesome name='check' size={16} color="#fff"/>
+                        )}
+                    </Pressable>
+
+                    <Text style={styles.textCheckbox}>Sua conta é organizacional?</Text>
+                </View>
             </View>
 
+            <TouchableOpacity style={styles.buttom} onPress={handleRegister}>
+                <Text style={styles.textButtom}>Criar Conta</Text>
+            </TouchableOpacity>
 
-            <Button title="Cadastrar" onPress={handleRegister} />
 
             <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                <Text style={styles.linkText}>Já tem uma conta? Fazer Login</Text>
+                <Text style={styles.linkText}>Fazer Login</Text>
             </TouchableOpacity>
 
 
@@ -138,29 +167,35 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 30,
         backgroundColor: '#fff',
-        paddingTop: 104,
+        paddingTop: 80,
     },
     title: {
-        fontSize: 48,
+        fontSize: 52,
         fontWeight: 'bold',
         marginBottom: 20,
         color: '#592E83',
         alignItems: 'flex-start'
 
     },
-    // form: {
-        
-    // },
+    form: {
+        width: '100%'
+    },
     input: {
         width: '100%',
         height: 50,
         borderColor: 'gray',
         borderRadius: 11,
-        paddingHorizontal: 10,
-        marginBottom: 15,
+        paddingHorizontal: 20,
+        marginBottom: 16,
         borderWidth: undefined,
         backgroundColor: '#F2EBFB',
-        paddingLeft: 20,
+        color: '#592E83'
+
+    },
+    titileInput: {
+        color: '#592E83',
+        fontSize: 18,
+        marginLeft: 10,
     },
 
     inputFocused: {
@@ -169,8 +204,10 @@ const styles = StyleSheet.create({
     },
 
     linkText: {
-        color: '#6200EE',
+        color: '#592E83',
         marginTop: 20,
+        fontWeight: 'bold',
+        fontSize: 24,
     },
 
     inputCheckbox: {
@@ -180,23 +217,47 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
 
+    containerCheckbox: {
+        width: '100%',
+        gap: 12,
+        marginTop: 20
+    },
+
     checkbox: {
-        width: 20,
-        height: 20,
-        borderRadius: 2,
-        borderWidth: 2,
-        borderColor: '#6200EE',
+        width: 24,
+        height: 24,
+        borderRadius: 4,
         alignItems: "center",
         backgroundColor: '#F2EBFB',
 
     },
     checkboxChecked: {
-        backgroundColor: '#6200EE',
+        backgroundColor: '#592E83',
         borderColor: '#F2EBFB',
-
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 
     textCheckbox: {
+        fontSize: 18,
+        color: '#592E83',
+        marginLeft: 10,
 
+    },
+
+    buttom: {
+        width: 270,
+        height: 50,
+        backgroundColor: '#F2EBFB',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 36,
+        borderRadius: 11,
+    },
+
+    textButtom: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#592E83'
     },
 });
