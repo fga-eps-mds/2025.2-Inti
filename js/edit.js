2afe2ecf30150db4f26e56b42b4eff76d6401fa4
Natan8643 <natan.j.franca@gmail.com>
2025-11-20 19:30:41 -0300

    Revert "[FEAT]: adiciona função de editar perfil"
    This reverts commit e83d7db28d09fd11824ba8c3827f221b2177bebb.

--- js/edit.js
+++ js/edit.js
@@ -1,29 +1,20 @@
-const API_CONFIG = {
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJub3ZvdCIsImV4cCI6MTc2NjA2MDc4MH0.VNz3bRuSpHtehwAIPPVVC3cJgxozc96qa7n82I7NiLs',
-    baseURL: 'https://20252-inti-production.up.railway.app/'
-};
-
-fetchUserData();
-
 async function fetchUserData() {
-    
+
     try {
-        const response = await fetch(`${API_CONFIG.baseURL}profile/me?size=5&page=5`, {
+
+        const response = await fetch('https://20252-inti-production.up.railway.app/', {
             method: 'GET',
             headers: {
-                'Authorization': `Bearer ${API_CONFIG.token}`,
-                'Content-Type': 'application/json'
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
             }
         });
 
-        console.log(response);
         if (!response.ok) {
             throw new Error(`Erro HTTP:  ${response.status}`);
         }
 
         const userData = await response.json();
-        populateForms(userData);
-        console.log(`${userData.email}`);
         return userData;
     }
 
@@ -36,16 +27,8 @@ async function fetchUserData() {
 
 function populateForms(userData) {
     document.getElementById('nome-input').value = userData.name || '';
-    //document.getElementById('email-input').value = userData.email || '';
+    document.getElementById('email-input').value = userData.email || '';
     document.getElementById('bio-input').value = userData.bio || '';
     //document.getElementById('telefone-input').value = userData.telefone || '';
 }
 
-
-async function editProfile() {
-    try {
-        const response = await fetch('http://localhost:8080/profile/')
-    } catch (error) {
-        
-    }
-}
