8b7fa2754cf3656fcd58a17ae15d7a605871c954
Lucas Santos <kelytonlucas@gmail.com>
2025-11-22 22:14:26 -0300

    fix: consertando imports

--- src/main/java/br/mds/inti/model/dto/EventRequestDTO.java
+++ src/main/java/br/mds/inti/model/dto/EventRequestDTO.java
@@ -1,47 +1,35 @@
 package br.mds.inti.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
-public record EventRequestDTO (
+public record EventRequestDTO(
 
-    @NotBlank // String não nula e não vazia
-    String title,
+        @NotBlank // String não nula e não vazia
+        String title,
 
-    @NotNull // não nulo
-    Instant eventTime,
+        @NotNull // não nulo
+        Instant eventTime,
 
-    @NotNull
-    String description,
+        @NotNull String description,
 
-    MultipartFile image,
+        MultipartFile image,
 
-    @NotNull
-    String streetAddress,
+        @NotNull String streetAddress,
 
-    @NotNull
-    String administrativeRegion,
+        @NotNull String administrativeRegion,
 
-    @NotNull
-    String city,
+        @NotNull String city,
 
-    @NotNull
-    String state,
+        @NotNull String state,
 
-    @NotNull
-    String referencePoint,
+        @NotNull String referencePoint,
 
-    @NotNull
-    BigDecimal latitude,
+        @NotNull BigDecimal latitude,
 
-    @NotNull
-    BigDecimal longitude
-){}
+        @NotNull BigDecimal longitude) {
+}
