e779c269766437db94b624e0fc3233ad73ce9f60
Lucas Santos <kelytonlucas@gmail.com>
2025-11-24 20:08:19 -0300

    fix: resolvendo schema do banco e consertando imports

--- src/main/java/br/mds/inti/controller/PostController.java
+++ src/main/java/br/mds/inti/controller/PostController.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
