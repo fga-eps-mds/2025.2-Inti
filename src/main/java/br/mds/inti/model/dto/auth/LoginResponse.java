c3090891a5fd5085350bda6df37e664e851010ca
Lucas Santos <kelytonlucas@gmail.com>
2025-12-08 16:16:55 -0300

    [FIX]: consertando retorno de auth

--- src/main/java/br/mds/inti/model/dto/auth/LoginResponse.java
+++ src/main/java/br/mds/inti/model/dto/auth/LoginResponse.java
@@ -1,11 +1,13 @@
 package br.mds.inti.model.dto.auth;
 
 import br.mds.inti.model.enums.ProfileType;
+import java.util.UUID;
 
 public record LoginResponse(
-        String jwt,
-        String username,
-        String name,
-        String email,
-        ProfileType type) {
+                UUID id,
+                String jwt,
+                String username,
+                String name,
+                String email,
+                ProfileType type) {
 }
