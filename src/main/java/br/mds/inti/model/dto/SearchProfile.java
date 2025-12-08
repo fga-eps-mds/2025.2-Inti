44ad9bac364d69eaaa327b3d6b4f8e2948d9269f
gustav <gustavuh0012@gmail.com>
2025-12-08 10:52:49 -0300

    feat: criação do dto, controller e services searchProfile

---
+++ src/main/java/br/mds/inti/model/dto/SearchProfile.java
@@ -0,0 +1,5 @@
+package br.mds.inti.model.dto;
+
+public record SearchProfile(String name, String username, 
+        String profile_picture_url){
+}
---
+++ src/main/java/br/mds/inti/service/SearchProfileService.java
@@ -0,0 +1,22 @@
+package br.mds.inti.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import br.mds.inti.model.dto.SearchProfile;
+import br.mds.inti.model.entity.Profile;
+import br.mds.inti.repositories.ProfileRepository;
+import br.mds.inti.service.exceptions.ProfileNotFoundException;
+
+@Service
+public class SearchProfileService {
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    public SearchProfile getProfileByUsername(String username){
+        Profile publicProfile = profileRepository.findByUsername(username)
+            .orElseThrow(() -> new ProfileNotFoundException(username));
+            
+        return new SearchProfile(publicProfile.getName(), publicProfile.getUsername(), publicProfile.getProfilePictureUrl());
+    }
+}
