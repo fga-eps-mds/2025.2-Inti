9ee8d40ed9b513c5728ba983e5f2aec9bd7ee5c8
Lucas Santos <kelytonlucas@gmail.com>
2025-12-11 17:46:41 -0300

    [FIX]: fixing import

--- src/main/java/br/mds/inti/model/dto/EventFollowingDTO.java
+++ src/main/java/br/mds/inti/model/dto/EventFollowingDTO.java
@@ -1,12 +1,9 @@
 package br.mds.inti.model.dto;
 
-import lombok.AllArgsConstructor;
-
 import java.util.UUID;
 
 public record EventFollowingDTO(
-        UUID profileId,
-        String username,
-        String profilePictureUrl
-) {
+                UUID profileId,
+                String username,
+                String profilePictureUrl) {
 }
